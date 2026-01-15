e3b8ac4d08cff0e9c1bf9d323e62254ba72ea558
Jonathan <jonathan.genet@gmail.com>
2026-01-15 12:05:50 +0100

    Fix: Enable Dark Mode and remove redundant actor info from popups

--- script.js
+++ script.js
@@ -371,8 +371,8 @@ function createCard(item) {
     let popupHTML = `<h3>${item.Activité}</h3>`;
     popupHTML += `<strong>Objectifs:</strong> ${item.Objectifs || 'N/A'}<br><br>`;
 
-    // Restore Actors
-    if (computedActors) popupHTML += `<strong>Acteurs:</strong> ${computedActors}<br>`;
+    // Restore Actors - Removed from popup as requested
+    // if (computedActors) popupHTML += `<strong>Acteurs:</strong> ${computedActors}<br>`;
 
     popupHTML += `<strong>Type de contrôle:</strong> ${item['Type de contrôle'] || 'N/A'}<br>`;
 
